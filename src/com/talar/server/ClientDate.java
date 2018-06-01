9971eeff50e28412ac623042e420c3866e88ce00
talarKoudoussian <talar.koudoussian@gmail.com>
2018-06-01 11:22:26 +0300

    feat: let server receive date requests from client

---
+++ src/com/talar/server/ClientDate.java
@@ -0,0 +1,64 @@
+package com.talar.server;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class ClientDate {
+
+        public static void main(String... args) throws UnknownHostException, IOException {
+
+                Socket socket = new Socket("localhost", 3000);
+                
+                BufferedOutputStream bos = new BufferedOutputStream(socket.getOutputStream());
+                BufferedInputStream bis = new BufferedInputStream(socket.getInputStream());
+                BufferedReader br = new BufferedReader(new InputStreamReader(bis));
+                
+                BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+
+                System.out.println("Enter a message");
+                System.out.println("List of commands: [Date, Date:ISO8601, DateTime:ISO8601, Date;format ,Time, exit, kill]");
+
+                boolean loop = true;
+                
+                while(loop) {
+                        String receiveMessage;
+                        String sendMessage = input.readLine();
+                        
+                        if(sendMessage.equals("exit") || sendMessage.equals("kill")) {
+                                loop = false;
+                        }
+                        
+                        sendMessage += "\0";
+                        
+                        bos.write(sendMessage.getBytes());
+                        bos.flush();
+        
+                        StringBuilder sb = new StringBuilder();
+                        
+                        while(true) {
+                                int c = br.read();
+        
+                                if(c == 0) {
+                                        break;
+                                }
+        
+                                sb.append((char)c);
+                        }
+                        
+                        receiveMessage = sb.toString();
+                        
+                        if(receiveMessage != null)
+                        {
+                                System.out.println("Received from server: " + receiveMessage);
+                        }
+                }
+                
+                socket.close();                
+        }
+
+}
--- src/com/talar/server/Server.java
+++ src/com/talar/server/Server.java
@@ -19,77 +19,81 @@ public class Server {
         private ServerSocket server = null;
         private BufferedInputStream bis = null;
         private BufferedOutputStream bos = null;
-        private boolean loop = false;
-        private Socket socket = null;
 
         public Server() {
 
         }
 
         public void start() {
-                socket = null;
-
+                Socket socket = null;
+                boolean loop = false;
+                
                 try {
                         server = new ServerSocket(port);
                         loop = true;
-
+                        
                         while(loop) {
                                 System.out.println("Server started on port " + getPort());
 
                                 socket = server.accept();
-
+                                
                                 InetAddress addr = socket.getInetAddress();
                                 String ipAddr = addr.getHostAddress();
                                 System.out.println("Client connected " + ipAddr + ":" + socket.getLocalPort());
-
-                                InputStream in = socket.getInputStream();
-                                OutputStream out = socket.getOutputStream();
-                                bis = new BufferedInputStream(in);
-                                bos = new BufferedOutputStream(out);
-
-                                int c = 0;
-                                StringBuilder sb = new StringBuilder();
-                                while(true) {
-                                        c = bis.read();
-
-                                        if(c == 0) {
-                                                break;
+                                
+                                boolean incoming = true;
+                                while(incoming) {
+                                        InputStream in = socket.getInputStream();
+                                        OutputStream out = socket.getOutputStream();
+                                        bis = new BufferedInputStream(in);
+                                        bos = new BufferedOutputStream(out);
+        
+                                        StringBuilder sb = new StringBuilder();
+                                        
+                                        while(true) {
+                                                int c = bis.read();
+        
+                                                if(c == 0) {
+                                                        break;
+                                                }
+        
+                                                sb.append((char)c);
                                         }
-
-                                        sb.append((char)c);
-                                }
-
-                                String received = sb.toString();
-                                String outMessage = "Server Received: \0" + sb.toString();
-                                System.out.println(outMessage);
-                                bos.write(received.getBytes());
-                                String response = respond(received); // getResponse(received);
-                                bos.write(response.getBytes());
-                                bos.flush();
-
-                                sb = null;
-
-                                switch(received) {
-                                        case "exit": {
-                                                System.out.println("Received: " + response);
-                                                bis.close();
-                                                bos.close();
-                                                socket.close();
-                                                break;
-                                        }
-
-                                        case "kill": {
-                                                System.out.println("Received: " + response);
-                                                bis.close();
-                                                bos.close();
-                                                socket.close();
-                                                loop = false;
-                                                break;
-                                        }
-
-                                        default: {
-                                                System.out.println("Received: " + response);
-                                                break;
+        
+                                        String receivedMessage = sb.toString();
+                                        String outMessage = "Server Received: \0" + sb.toString();
+                                        System.out.println(outMessage);
+                                        String response = getResponse(receivedMessage);
+                                        response += "\0";
+                                        bos.write(response.getBytes());
+                                        bos.flush();
+        
+                                        sb = null;
+        
+                                        switch(receivedMessage) {
+                                                case "exit": {
+                                                        System.out.println("Received Response: " + response);
+                                                        bis.close();
+                                                        bos.close();
+                                                        socket.close();
+                                                        incoming = false;
+                                                        break;
+                                                }
+        
+                                                case "kill": {
+                                                        System.out.println("Received Response: " + response);
+                                                        bis.close();
+                                                        bos.close();
+                                                        socket.close();
+                                                        incoming = false;
+                                                        loop = false;
+                                                        break;
+                                                }
+        
+                                                default: {
+                                                        System.out.println("Received Response: " + response);
+                                                        break;
+                                                }
                                         }
                                 }
                         }
@@ -120,6 +124,7 @@ public class Server {
                 }
 
                 System.out.println("Shutting down Socket server!!");
+                
                 try {
                         server.close();
                 }
@@ -138,19 +143,32 @@ public class Server {
 
         private String getResponse(String message) {
                 String response = "";
+                String format = "";
+                
+                if(isFormatDefined(message)) {
+                        format = getDateFormat(message);
+                        message = "Date;";
+                }
+               
                 switch(message) {
-                        case "hello": {
-                                response = "Hello how can I help?";
+                        case "Date": {
+                                response = getDate();
                                 break;
                         }
-                        case "time": {
-                                SimpleDateFormat sdf = new SimpleDateFormat("h:mm a");
-                                Date date = new Date();
-                                response = sdf.format(date);
+                        case "Date:ISO8601": {
+                                response = getISODate();
                                 break;
                         }
-                        case "thank you": {
-                                response = "You're welcome";
+                        case "DateTime:ISO8601": {
+                                response = getISODateTime();
+                                break;
+                        }
+                        case "Date;" : {
+                                response = getCustomDate(format);
+                                break;
+                        }
+                        case "Time" : {
+                                response = getTime();
                                 break;
                         }
                         case "exit": {
@@ -159,32 +177,6 @@ public class Server {
                         }
                         case "kill": {
                                 response = "killing..";
-                                break;
-                        }
-                        default: {
-                                response = "Message unknown";
-                                break;
-                        }
-                }
-
-                return response;
-        }
-        
-        private String respond(String message) {
-                message = message.toLowerCase();
-                String response = "";
-               
-                switch(message) {
-                        case "date": {
-                                response = getDate();
-                                break;
-                        }
-                        case "date:iso8601": {
-                                response = getISODate();
-                                break;
-                        }
-                        case "datetime:iso8601": {
-                                response = getISODateTime();
                                 break;
                         }
                         default: {
@@ -197,7 +189,7 @@ public class Server {
         }
         
         private String getDate() {
-                SimpleDateFormat sdf = new SimpleDateFormat("h:mm a");
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
                 Date date = new Date();
                 String response = sdf.format(date);
                 return response;
@@ -219,6 +211,43 @@ public class Server {
                 return response;
         }
         
+        private String getCustomDate(String format) {
+                String response = "";
+                LocalDateTime dateNow = LocalDateTime.now();
+                DateTimeFormatter dtf = DateTimeFormatter.ofPattern(format);
+                response = dateNow.format(dtf);
+                return response;
+        }
+        
+        private String getDateFormat(String message) {
+                String dateFormat = "";
+                
+                if(isFormatDefined(message)) {
+                        String[] args = message.split(";");
+                        dateFormat = args[1];
+                }
+                
+                return dateFormat;
+        }
+        
+        private boolean isFormatDefined(String message) {
+                boolean isDefined = false;
+                String[] args = message.split(";");
+                
+                if(args.length > 1) {
+                        isDefined = true;
+                }
+                
+                return isDefined;
+        }
+        
+        private String getTime() {
+                SimpleDateFormat sdf = new SimpleDateFormat("h:mm a");
+                Date date = new Date();
+                String response = sdf.format(date);
+                return response;
+        }
+
         public static void main(String... args) {
                 Server srv = new Server();
                 srv.setPort(3000);
